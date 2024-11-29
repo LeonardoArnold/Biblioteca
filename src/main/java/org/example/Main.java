f2eeb091a17b00a7441b43ccc574cc2ca0a0f6f6
LeonardoArnold <leonardobandasshow@gmail.com>
2024-11-29 11:59:45 -0300

    teste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Teste de cadastro de autor
-        CadastroAutor.cadastrarAutor("Pedro", "Piadas");
-        CadastroAutor.cadastrarAutor("Maria", "Oliveira");
+        CadastroAutor.cadastrarAutor("João", "Victor");
     }
 }
